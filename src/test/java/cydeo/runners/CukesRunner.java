6d6ce214871ebcd962ece1ef8ef5a56ad199e6e6
mehmetCt <mehmet@cybertekschool.com>
2023-08-14 12:12:33 -0400

    Selenium Grid 4 init

--- src/test/java/cydeo/runners/CukesRunner.java
+++ src/test/java/cydeo/runners/CukesRunner.java
@@ -8,9 +8,9 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         plugin = {
                 "json:target/cucumber.json",
-                "html:target/cucumber/report.html",
+                "html:target/cucumber-reports.html",
                 "me.jvt.cucumber.report.PrettyReports:target/cucumber",
-                "rerun:target/rerun.txt",},
+                },
         features = "src/test/resources/features",
         glue = "cydeo/steps",
         publish = true
